c999b4944aff79ad318dbe52e7b7fcf0146515f6
NicoSerranoP <nnicosp@hotmail.com>
2026-03-11 10:39:12 -0500

    fix(nextjs): format types and lint

--- packages/nextjs/contracts/externalContracts.ts
+++ packages/nextjs/contracts/externalContracts.ts
@@ -11,6 +11,89 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
  *   },
  * } as const;
  */
-const externalContracts = {} as const;
+const externalContracts = {
+	31337: {
+		SE2Token: {
+			// Replace with your deployed SE2Token address when available.
+			address: "0x0000000000000000000000000000000000000000",
+			abi: [
+				{
+					inputs: [
+						{
+							internalType: "address",
+							name: "account",
+							type: "address",
+						},
+					],
+					name: "balanceOf",
+					outputs: [
+						{
+							internalType: "uint256",
+							name: "",
+							type: "uint256",
+						},
+					],
+					stateMutability: "view",
+					type: "function",
+				},
+				{
+					inputs: [],
+					name: "totalSupply",
+					outputs: [
+						{
+							internalType: "uint256",
+							name: "",
+							type: "uint256",
+						},
+					],
+					stateMutability: "view",
+					type: "function",
+				},
+				{
+					inputs: [
+						{
+							internalType: "address",
+							name: "to",
+							type: "address",
+						},
+						{
+							internalType: "uint256",
+							name: "amount",
+							type: "uint256",
+						},
+					],
+					name: "mint",
+					outputs: [],
+					stateMutability: "nonpayable",
+					type: "function",
+				},
+				{
+					inputs: [
+						{
+							internalType: "address",
+							name: "to",
+							type: "address",
+						},
+						{
+							internalType: "uint256",
+							name: "amount",
+							type: "uint256",
+						},
+					],
+					name: "transfer",
+					outputs: [
+						{
+							internalType: "bool",
+							name: "",
+							type: "bool",
+						},
+					],
+					stateMutability: "nonpayable",
+					type: "function",
+				},
+			],
+		},
+	},
+} as const;
 
 export default externalContracts satisfies GenericContractsDeclaration;
